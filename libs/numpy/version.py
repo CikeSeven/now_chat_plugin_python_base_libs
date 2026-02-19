ec35ac24c965424dcb1863e3d4520234356b6207
cikeseven <llyllydyx@163.com>
2026-02-20 00:26:33 +0800

    切换到3.10版本

--- libs/numpy/version.py
+++ libs/numpy/version.py
@@ -3,6 +3,6 @@ version = "1.26.2"
 __version__ = version
 full_version = version
 
-git_revision = "9ac93b26a92b25f77a594a7a5bca6db9150ddf48"
+git_revision = "5896ca5ec5856638324b3c77c65c1ef708dc5a1a"
 release = 'dev' not in version and '+' not in version
 short_version = version.split("+")[0]
