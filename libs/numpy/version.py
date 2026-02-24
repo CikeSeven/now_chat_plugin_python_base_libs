9460fb6b20713e996b21d84055ac81e8d6276861
cikeseven <llyllydyx@163.com>
2026-02-24 23:11:39 +0800

    feat: 适配新插件安装方式

--- libs/numpy/version.py
+++
@@ -1,8 +0,0 @@
-
-version = "1.26.2"
-__version__ = version
-full_version = version
-
-git_revision = "5896ca5ec5856638324b3c77c65c1ef708dc5a1a"
-release = 'dev' not in version and '+' not in version
-short_version = version.split("+")[0]
